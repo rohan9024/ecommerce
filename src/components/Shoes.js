6f182de1fe79dfe467bf7edfae01384179f6e158
chintan-golakiya <32306937+chintan-golakiya@users.noreply.github.com>
2022-12-05 23:47:56 +0530

    added shoes router and added links in header

---
+++ src/components/Shoes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../components/Header";
+import Fifth from './Fifth';
+import Footer from './Footer';
+
+function Shoes() {
+  return (<div>
+    <Header/>
+    <Fifth/>
+    <Footer/>
+  </div>);
+}
+
+export default Shoes
