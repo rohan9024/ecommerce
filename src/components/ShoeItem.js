35117f3d52367b03fe5edceea54c29ef9a93630a
Dibyendu Dey <dibyendudey303@gmail.com>
2022-12-08 18:41:19 +0530

    added more shoes

---
+++ src/components/ShoeItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import cart from "../assets/cart.png"
+import { motion } from "framer-motion"
+const ShoeItem = (props) => {
+    const { title, desc, img, price } = props;
+    return (
+        <motion.div
+            className='shadow-sm border border-gray-200 transition hover:scale-105 hover:ease-in-out hover:duration-500 hover:delay-125 hover:shadow-lg cursor-pointer'>
+            <img src={img} alt="img6" className='h-[450px] w-[408px] object-cover' />
+            <div className='flex justify-between items-center w-[408px] h-[130px] px-4 pb-5'>
+                <div className='flex flex-col space-y-3'>
+                    <h1 className='font-bold text-lg mt-5'>{title}</h1>
+                    <h3 className='text-sm font-normal text-gray-700'>{desc}</h3>
+                    <h1>{price}</h1>
+                </div>
+                <div className='flex justify-center items-center '>
+                    <img
+                        className='object-contain w-5 h-5'
+                        src={cart} alt="cart" />
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default ShoeItem
