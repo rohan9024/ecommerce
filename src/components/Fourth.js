913d54ccac027156dd5ea4eac7ef048c58375524
Devika <devikas.iitg@gmail.com>
2022-12-12 13:57:48 +0530

    Added scroll based text animations on homepage

--- src/components/Fourth.js
+++ src/components/Fourth.js
@@ -5,19 +5,35 @@ import men from "../assets/men.webp"
 import women from "../assets/women.webp"
 import kids from "../assets/kids.webp"
 import cart from "../assets/cart.png"
-import { motion } from "framer-motion"
+import { useEffect } from "react"
+import { motion , useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+import animateText from "./AnimateHomeText"
 import { Link } from "react-router-dom"
 
 function Fourth() {
 
+    //code to animate heading 
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+  
+    useEffect(() => {
+        if (inView) {
+          control.start("visible");
+        } else {
+          control.start("hidden");
+        }
+      }, [control, inView]);
+    
 
     return (
         <div className='w-full mx-auto font-dmsans flex flex-col justify-center items-center my-10'>
             <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
-                className='flex flex-row space-x-2 justify-center items-center'>
+                className='flex flex-row space-x-2 justify-center items-center'
+                ref={ref}
+                variants={animateText}
+                initial="hidden"
+                animate={control}>
                 <h1 className='text-black text-sm sm:text-xl md:text-2xl lg:text-4xl font-bold'>Explore Categories</h1>
             </motion.div>
             <div className='w-5/6 mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-5'>
