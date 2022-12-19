e2ec4541a16b48d21aee8e2a5304d536e29813cf
Dibyendu Dey <dibyendudey303@gmail.com>
2022-12-19 20:19:12 +0530

    Improved sort button UI

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { FaAngleDown } from "react-icons/fa";
 
 const Sort = (props) => {
     const { setSortValue, sortValue } = props;
     return (
         <div className='flex justify-end p-4 relative'>
             <div className='group w-72  p-2 cursor-pointer text-center absolute bg-white'>
-                <h3 className='font-semibold text-xl'>Sort By: <span className='font-normal'>{sortValue}</span></h3>
+                <div className="heading flex justify-center items-center">
+                    <h3 className='font-semibold text-xl'>Sort By {sortValue && ": "} </h3>
+                    <span className='font-normal'>{sortValue}</span>
+                    <div className='grid place-items-center ml-3'> <FaAngleDown /></div>
+                </div>
                 <ul className='hidden group-hover:block pt-4 shadow-2xl'>
                     <li className='pt-3 hover:font-semibold' onClick={() => setSortValue("Price: Low to High")} id="lowest">Price: Low to High</li>
                     <li className='pt-3 hover:font-semibold' onClick={() => setSortValue("Price: High to Low")} id="highest">Price: High to Low</li>
