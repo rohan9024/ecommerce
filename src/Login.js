b5e4904d7e3728f6940abd35eb347fa40a45bd26
Yash Ainapure <ainapureyash2@gmail.com>
2023-12-19 09:44:10 +0530

    validate login and signup form

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from "./assets/logo.png"
 import { ToastContainer, toast } from 'react-toastify';
@@ -7,6 +7,14 @@ import 'react-toastify/dist/ReactToastify.css';
 function Login() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
   const notifySuccess = () => toast.success('Logged in successfully', {
     position: "top-right",
     autoClose: 2000,
@@ -27,7 +35,52 @@ function Login() {
     progress: undefined,
     theme: "light",
   });
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      notifySuccess();
+    } else {
+      notifyError();
+    }
+  }
 
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = { ...errors };
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+      isValid = false;
+    } else if (!isValidEmail(formData.email)) {
+      newErrors.email = "Invalid email format";
+      isValid = false;
+    } else {
+      newErrors.email = "";
+    }
+
+    if (formData.password.length < 5) {
+      newErrors.password = "Password must be at least 5 characters";
+      isValid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   return (
     <>
@@ -50,13 +103,17 @@ function Login() {
             src={logo} alt="logo" />
           <h1 className='font-bold'>YOUR ACCOUNT FOR EVERYTHING BLACKBERRY </h1>
           <form className='flex flex-col justify-center items-center space-y-5'>
-            <input ref={emailRef} type="email" placeholder="Email address" className='placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-72' />
-            <input ref={passwordRef} type="password" placeholder="Password" className='placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-72' />
+            <input ref={emailRef} type="email" placeholder="Email address" name='email'
+              onChange={handleInputChange} className='placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-72' />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
+            <input ref={passwordRef} type="password" placeholder="Password" name='password'
+              onChange={handleInputChange} className='placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-72' />
+            {errors.password && <p className="text-red-500">{errors.password}</p>}
             <div className='flex justify-between items-center'>
               <h1 className='font-normal text-sm text-right ml-24 text-gray-500'>Forgot your Password?</h1>
             </div>
-            <div className='flex justify-center items-center w-72 bg-black text-white py-2'>
-              <button type='submit'>Sign In</button>
+            <div className='flex justify-center items-center w-72 bg-black text-white py-2' onClick={handleLogin}>
+              <button>Sign In</button>
             </div>
             <div className='flex justify-center items-center space-x-1'>
               <h1 className='text-gray-500'>Not a member?</h1>
